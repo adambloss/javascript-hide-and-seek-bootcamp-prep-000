41c939ddf7759eb1846bdfb340dc67930710fb31
Adam Bloss <adam.bloss17@gmail.com>
2018-08-07 17:18:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+function getFirstSelector(selector){
+    return document.querySelector('selector')
+}
+function nestedTarget(){
+ a = document.querySelector('div.target')
+return a.getElementsByTagName('div')[0]
+}
